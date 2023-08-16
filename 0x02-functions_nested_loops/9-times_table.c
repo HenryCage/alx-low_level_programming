d94168c61d6692ed6dcf57ddf59d08b030a1d1fd
Omo-Bare Emmanuel <henry12cage@gmail.com>
2023-08-15 19:09:30 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - multiplication table
+ */
+void times_table(void)
+{
+	int i, j, x;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0 ; j < 10 ; j++)
+		{
+			x = i * j;
+			if (j == 0)
+				printf("%d, ", x);
+			else
+			{
+				printf("%2d", x);
+				if (j != 9)
+					printf(", ");
+			}
+		}
+		printf("\n");
+	}
+}
