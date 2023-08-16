f717673fd99122a1c3ad98934e56fbec899effb3
Omo-Bare Emmanuel <henry12cage@gmail.com>
2023-08-15 17:31:23 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - absolute values
+ * @p: parameter to be checked
+ * Return: returns absolute value
+ */
+int _abs(int p)
+{
+	if (p < 0)
+	{
+		return (-p);
+	}
+	else
+	{
+		return (p);
+	}
+}
