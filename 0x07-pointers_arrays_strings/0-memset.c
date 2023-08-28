c6d3b08ccf6fc3e331c1a7d41084bb8b6d800e2a
Omo-Bare Emmanuel <henry12cage@gmail.com>
2023-08-28 15:54:46 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,17 @@
 #include "main.h"
+/**
+ * _memset - fill memory
+ * @s: first parameter
+ * @b: second parameter
+ * @n: unsigned int
+ * Return: returns s
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0 ; i < n ; i++)
+		s[i] = b;
+	return (s);
 }
