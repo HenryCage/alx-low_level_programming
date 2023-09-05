f0a95453a0ec071f025d22dd3b2b364d6e7560f0
Omo-Bare Emmanuel <henry12cage@gmail.com>
2023-09-05 14:30:15 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - the main program
+ * @argc: number of arguments
+ * @argv: the array of the arguments
+ * Return: 0 when successful
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
