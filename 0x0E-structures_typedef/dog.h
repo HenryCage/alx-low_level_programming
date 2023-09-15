836a04a08bf07f4b306731f7bdffb019a4292b74
Omo-Bare Emmanuel <henry12cage@gmail.com>
2023-09-15 06:09:15 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - the new structure
+ * @name: the first parameter
+ * @age: the second parameter
+ * @owner: the last parameter
+ */
 struct dog
 {
 	char *name;
