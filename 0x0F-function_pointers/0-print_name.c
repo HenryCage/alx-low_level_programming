e39eab5ea73e5f4af8d7b079a1fe0b087bbaf797
Omo-Bare Emmanuel <henry12cage@gmail.com>
2023-09-15 18:16:54 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints the name
+ * @name: the first parameter
+ * @f: second one
+ * Return: null
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
