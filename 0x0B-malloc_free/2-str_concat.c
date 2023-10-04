6900cc4715bcb21fe81944f8ccb4081d824083ff
Omo-Bare Emmanuel <henry12cage@gmail.com>
2023-10-04 06:42:18 -0700

    Concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,18 +25,18 @@ char *str_concat(char *s1, char *s2)
 
 	if (string == NULL)
 		return (NULL);
-
 	i = j = 0;
 	while (s1[i] != '\0')
 	{
 		string[i] = s1[i];
 		i++;
 	}
+
 	while (s2[j] != '\0')
 	{
-		string[j] = s2[j];
-		j++;
+		string[i] = s2[j];
+		i++, j++;
 	}
-	/**string[i] != '\0';*/
+	string[i] = '\0';
 	return (string);
 }
