662a1ac6cae22e2dfb02261926f72a2a141d72d9
Omo-Bare Emmanuel <henry12cage@gmail.com>
2023-10-12 02:29:51 -0700

    print all numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that prints numbers
+ * @separator: the string to be printed in between numbers
+ * @n: number of integers passed
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list a;
+	unsigned int i;
+
+	va_start(a, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(a, int));
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(a);
+}
