cd4431c8ae437c08264a6c7ce3260170149cdb62
Omo-Bare Emmanuel <henry12cage@gmail.com>
2023-10-12 07:23:25 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - the dogs informaton
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
