52591cc2776ad6c8d21267e1040a7a73187071e4
Omo-Bare Emmanuel <henry12cage@gmail.com>
2023-10-23 16:21:02 -0700

    Print the nodes in the list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t i = 0;
+	size_t no_nodes = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		i++;
+		no_nodes++;
 		h = h->next;
 	}
 
-	return (i);
+	return (no_nodes);
 }
