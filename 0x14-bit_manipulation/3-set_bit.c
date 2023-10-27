a2b38ef3a12ce3346ebc1119f887122fb0485c0b
Omo-Bare Emmanuel <henry12cage@gmail.com>
2023-10-26 21:17:04 -0700

    tome the npne losrmennn

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * orc said 
+ *
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i;
+
+	if (index > 63)
+		return (-1);
+
+	i = 1 << index;
+	*n ( *n | m);
+
+	return (1);
+}
