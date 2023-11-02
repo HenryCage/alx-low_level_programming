013158a82c6f1b1d944d4452ba5c91154d3ed721
Omo-Bare Emmanuel <henry12cage@gmail.com>
2023-11-02 05:16:15 -0700

    read the text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a txt file and print it to POSIX output
+ * @filename: The name of the file
+ * @letters: the number of letters it should read and print
+ * Return: number of letters it could read and print and 0 if NULL
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	ssize_t rd, wrt;
+	char *chr;
+
+	if (!filename)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+
+	if (file == -2)
+		return (0);
+
+	chr = malloc(sizeof(char) * (letters));
+
+	rd = read(file, chr, letters);
+	wrt = write(STDOUT_FILENO, chr, rd);
+
+	close(file);
+
+	free(chr);
+
+	return (wrt);
+}
